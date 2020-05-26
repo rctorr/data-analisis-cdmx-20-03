28e282ed63b39c31ae9c0bffea428fdc6e67fb99
Ricardo Torres <rictor@cuhrt.com>
2020-05-26 17:03:35 -0500

    Agregando notas a la Sesion-01, script hola.py y script recorridos.py solucion que imprime la tabla con formato

---
+++ Sesion-01/recorridos.py
@@ -0,0 +1,17 @@
+linea = "-" * 55
+formato1 = "{:15} | {:15} | {:10}"
+formato2 = "{:15} | {:15} | {:>10}"
+formato3 = "{:15} | {:>15} | {:>10}"
+
+print()
+print(linea)
+print(formato1.format("ORIGEN", "DESTINO", "TIEMPO"))
+print(linea)
+print(formato2.format("Roma Norte", "Tabacalera", "15:00"))
+print(formato2.format("Reforma", "Juárez", "8:00"))
+print(formato2.format("Alameda", "Condesa", "20:00"))
+print(formato2.format("Roma Sur", "Roma Norte", "4:00"))
+print(formato2.format("Buenavista", "Del Valle Norte", "30:00"))
+print(linea)
+print(formato3.format("", "Tiempo total:", "1:17:00"))
+print()
