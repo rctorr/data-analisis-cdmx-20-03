af13614fd355c983c75c10c9ba6faa9a32513f9d
Ricardo Torres <rictor@cuhrt.com>
2020-06-02 22:14:21 -0500

    Agregando la columna de fecha de modificacion a la lista de elementos del script le.py

--- Sesion-03/le.py
+++ Sesion-03/le.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 
@@ -9,25 +10,30 @@ def obtener_elementos(carp):
     
     for nombre in nombres:
         tam = os.path.getsize(nombre)
-        elem = [nombre, tam]
+        fecha = os.path.getmtime(nombre)  # fecha en timestamp o epoc | 1970-01-01
+        fecha = datetime.datetime.fromtimestamp(fecha)  # fecha python
+        elem = [nombre, tam, fecha]
         elementos.append(elem)
     
     return elementos
 
 def imprime_txt(elems):
     """ Imprime la lista de elementos en formato txt """
-    print("{:10}  {}".format("TAMAÑO", "NOMBRE") )
+    print("{:10}  {:19}  {}".format("TAMAÑO", "FECHA", "NOMBRE") )
     for e in elems:
-        print("{:10}  {}".format(e[1], e[0]) )
+        fecha = e[2].strftime("%Y-%m-%d %H:%M:%S")
+        print("{:10}  {:19}  {}".format(e[1], fecha, e[0]) )
 
 
 carpeta = "."
 
-# Leer lista de elementos con nombre y tamaño
+# Leer lista de elementos con nombre, tamaño y fecha
 elementos = obtener_elementos(carpeta)
 
 # Ordenar los elementos por nombre
-elementos.sort(key=lambda x: x[0].lower() )  # x -> ["nom", 1234], x[0]
+# elementos.sort(key=lambda x: x[0].lower() )  # x -> ["nom", 1234], x[0]
+# Ordenar los elementos por fecha, el más reciente primeo
+elementos.sort(key=lambda x: x[0].lower() )  # x -> ["nom", 1234, "fecha"], x[0]
 
 # Imprime lista de elementos en formato txt
 imprime_txt(elementos)
