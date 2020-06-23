2d30399d49bc9f980c9dd2656295c5e86d5bc923
Ricardo Torres <rictor@cuhrt.com>
2020-06-22 20:43:14 -0500

    Actualizando primer_arania.py para navegar en todas la paginas de citas

--- Sesion-07/primer_arania.py
+++ Sesion-07/primer_arania.py
@@ -8,11 +8,18 @@ class CitasArania(scrapy.Spider):
         "http://quotes.toscrape.com/",
     ]
     
-    # Que datos y como lo vamos procesar
+    # Que datos y como lo vamos procesar para todas las citas
     def parse(self, respuesta):
         # Lista de todas las citas
         for cita in respuesta.css("div.quote"):
+            # Filtramos datos para cada una
             yield {
                 "texto" : cita.css("span.text::text").get(),
                 "autor" : cita.xpath("span/small/text()").get(),
             }
+
+        # Obtener el elemento que nos lleva a la página siguiente
+        pag_sig = respuesta.css("li.next a::attr('href')").get()
+        if pag_sig is not None:  # si pag_sig existe!
+            yield respuesta.follow(pag_sig, self.parse)
+        
