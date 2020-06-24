dc2c73608a5a23861c03e57c21e271eb6dade7db
Ricardo Torres <rictor@cuhrt.com>
2020-06-23 20:02:06 -0500

    Agregando ejemplo para matplotlib

---
+++ Sesion-08/lineas.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+# Valores para los ejes
+x = [1,2,3,4,5,6,7,8,9,10]
+y = (1.73, 1.55, 1.72, 1.68, 1.70, 1.58, 1.53, 1.65, 1.78, 1.64)
+
+# Crear la gráfica
+plt.plot(x, y)
+
+# Visualizar gráfica
+plt.show()
